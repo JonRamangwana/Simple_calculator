adac6b86ae3f039637dadb9819c89c04d0665624
JonRamangwana <jonramangwana@outlook.com>
2022-10-15 12:46:39 +0200

    Add files via upload

---
+++ calculator.py
@@ -0,0 +1,16 @@
+#Calculator
+
+first_num = float(input("Enter first number: "))
+op = input("Enter operator: ")
+second_num = float((input("Enter second number: ")))
+
+if op == "+":
+    print(first_num + second_num)
+elif op == "-":
+    print(first_num + second_num)
+elif op == "/":
+    print(first_num / second_num)
+elif op == "*":
+    print(first_num * second_num)
+else:
+    print("Invalid operator")
